d46b6ea74eb1b4589d218da9daa604a3d2d4d83c
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2024-11-10 20:22:54 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import MainContainer from "./components/mainContainer";
 import SideBar from "./components/sideBar";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
+    <div
+      className={styles.app}
+      style={{ display: "flex", width: "100vw", height: "100vh" }}
+    >
       <SideBar />
       <MainContainer />
     </div>
